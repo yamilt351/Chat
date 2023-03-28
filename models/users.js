57905a2846786adf1dceffc5be6d0e9a7da9387c
yamil351 <leokasper@live.com.ar>
2023-03-28 20:41:56 -0300

    user-model-updated-to-allow-social-media-integration

--- models/users.js
+++ models/users.js
@@ -1,94 +1,107 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePictureId: {
-    type: String,
-  },
-	authToken: {
-			type:Number,
-	},
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  notification: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Notification",
-  },
-  chatGroups: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "ChatGroup",
-    },
-  ],
-  online: {
-    status: {
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
       type: String,
-      enum: ["active", "away", "dnd", "offline"],
-      default: "active",
+      required: true,
+      unique: true,
     },
-    lastActive: {
-      type: Date,
-      default: Date.now,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  },
-  reaction: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Reaction",
-  },
-  description: {
-    type: String,
-    default: "",
-  },
-  name: {
-    type: String,
-    default: "",
-  },
-  age: {
-    type: Number,
-    default: 0,
-  },
-  links: {
-    type: [String],
-  },
-  relationships: [
-    {
-      userId: {
+    password: {
+      type: String,
+      required: true,
+    },
+    imageId: {
+      type: String,
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    subscribedUser: {
+      type: [String],
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    notification: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Notification",
+    },
+    readLater: {
+      type: [String],
+    },
+    chatNotification: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Notification",
+    },
+    chatGroups: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+        ref: "ChatGroup",
       },
-      type: {
-        type: String,
-        enum: ["friend", "group"],
-        required: true,
-      },
+    ],
+    online: {
       status: {
         type: String,
-        enum: ["pending", "accepted", "blocked"],
-        required: true,
+        enum: ["active", "away", "dnd", "offline"],
+        default: "active",
+      },
+      lastActive: {
+        type: Date,
+        default: Date.now,
       },
     },
-  ],
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
+    reaction: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reaction",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    age: {
+      type: Number,
+      default: 0,
+    },
+    links: {
+      type: [String],
+    },
+    relationships: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        type: {
+          type: String,
+          enum: ["friend", "group"],
+          required: true,
+        },
+        status: {
+          type: String,
+          enum: ["pending", "accepted", "blocked"],
+          required: true,
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-});
-
+  { timestamp: true }
+);
 export default mongoose.model("User", UserSchema);
